97d041a595fbdd72be086b4533af11f9f36a3913
Jordy <arno.zheng@gmail.com>
2019-12-05 15:47:29 +0800

    fix: MemcacheException

--- MemcacheException.php
+++ MemcacheException.php
@@ -8,7 +8,41 @@
 
 namespace All\Exception;
 
-class MemcacheException extends RedisException
+class MemcacheException extends Exception
 {
+    protected $method = '';
+    protected $params = [];
+    protected $config = [];
 
+    public function setMethod($method)
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function setParams($params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
 }
